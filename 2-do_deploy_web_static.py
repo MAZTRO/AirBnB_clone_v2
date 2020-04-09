5fad3f8dd76247b6aceb1b6cb0294f6eac2d2f08
MAZTRO <design.mallet@gmail.com>
2020-04-09 22:13:03 +0000

    Change all

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,20 +14,18 @@ env.password = "MAZTRO"
 def do_deploy(archive_path):
     """ deploy a file """
     if (os.path.isfile(archive_path)):
-        com = "tar -xzf"
-        pth_re = "/data/web_static/releases/"
-        pth_cur = "/data/web_static/current"
         fls = archive_path.split("/")
         fld = fls[-1].split(".")
-
         put(archive_path, "/tmp/")
-        sudo("mkdir -p {}{}".format(pth_re, fld[0]))
-        sudo("{} /tmp/{} -C {}{}/".format(com, fls[-1], pth_re, fld[0]))
+        sudo("mkdir -p /data/web_static/releases/{}/".format(fld[0]))
+        data = "/data/web_static/releases"
+        sudo("tar -xzf /tmp/{} -C {}/{}/".format(fls[-1], data, fld[0]))
         sudo("rm /tmp/{}".format(fls[-1]))
-        sudo("mv {0}{1}/web_static/* {0}{1}".format(pth_re, fld[0]))
-        sudo("rm -rf {}{}/web_static".format(pth_re, fld[0]))
+        path_mv = "mv /data/web_static/releases"
+        sudo("{}/{}/web_static/* {}/{}/".format(path_mv, fld[0], data, fld[0]))
+        sudo("rm -rf {}/{}//web_static".format(data, fld[0]))
         sudo("rm -rf /data/web_static/current")
-        sudo("ln -s {}{} {}".format(pth_re, fld[0], pth_cur))
+        sudo("ln -s {}/{}/ /data/web_static/current".format(data, fld[0]))
         print("New version deployed!")
         return True
     else:
