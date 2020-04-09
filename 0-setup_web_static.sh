b901afb1ca76037e6715e0fb199fa17cc2d7146b
MAZTRO <design.mallet@gmail.com>
2020-04-09 19:13:51 +0000

    remove -e

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,8 @@ sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    ** Holberton School ** \n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "** Holberton School **" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "/root \/var\/www\/html;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
