58905def6b30114d41a321799ca4ff1d69ae1c6b
MAZTRO <design.mallet@gmail.com>
2020-04-09 19:31:16 +0000

    change specific line in default file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,5 +10,5 @@ sudo mkdir -p /data/web_static/releases/test/
 echo "** Holberton School **" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/root \/var\/www\/html;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo sed -i "43 a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
