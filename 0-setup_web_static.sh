dc8fccb30d414d4e4305d4bbf4773b804dcaf19a
MAZTRO <design.mallet@gmail.com>
2020-04-09 18:51:20 +0000

    Add sudo to 'sed' command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,5 +10,5 @@ sudo mkdir -p /data/web_static/releases/test/
 echo -e "<html>\n  <head>\n  </head>\n  <body>\n    ** Holberton School ** \n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i "/root \/var\/www\/html;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo sed -i "/root \/var\/www\/html;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
