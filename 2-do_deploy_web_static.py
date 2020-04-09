495683b13bc6c81517f6f29bfc07d015f1d284e8
MAZTRO <design.mallet@gmail.com>
2020-04-09 20:17:34 +0000

    Add chown command

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,6 +27,7 @@ def do_deploy(archive_path):
         sudo("rm -rf {}{}/web_static".format(pth_re, fld[0]))
         sudo("rm -rf /data/web_static/current")
         sudo("ln -s {}{}/ {}".format(pth_re, fld[0], pth_cur))
+        sudo("sudo chown -R ubuntu:ubuntu /data/")
 
         print("New version deployed!")
         return True
