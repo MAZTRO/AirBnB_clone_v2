1f754227682b70fb494ffbee659225a6d81fa00d
MAZTRO <design.mallet@gmail.com>
2020-04-10 01:55:22 +0000

    add last task

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Setup a web static with puppet
+exec { 'web_static':
+command  => 'sudo apt-get update && sudo apt-get install -y nginx && sudo mkdir -p /data/ && sudo mkdir -p /data/web_static/ && sudo mkdir -p /data/web_static/releases/ && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && sudo touch /data/web_static/releases/test/index.html && echo "** Holberton School **" | sudo tee /data/web_static/releases/test/index.html && sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i "43 a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default && sudo service nginx restart',
+provider => shell,
+}
