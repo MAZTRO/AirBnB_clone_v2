9f26c9a7da46df6357ed41e282c0262fbc4cbea2
MAZTRO <design.mallet@gmail.com>
2020-04-10 17:24:04 +0000

    remove import

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,8 +4,7 @@ Script to compress and deploy a file
 with Fabric into several servers
 """
 from fabric.api import *
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+from datetime import datetime
 import os
 
 env.hosts = [
@@ -15,6 +14,47 @@ env.hosts = [
 env.password = "MAZTRO"
 
 
+@runs_once
+def do_pack():
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    name = "web_static_{}.tgz".format(time)
+
+    # local('mkdir -p versions')
+    print("Packing web_static to versions/{}".format(name))
+    command = "mkdir -p versions && tar -cvzf"
+    output = local('{} versions/{} web_static'.format(command, name))
+    if (output.succeeded):
+        size = os.path.getsize("versions/{}".format(name))
+        print("web_static packed: versions/{} -> {}Bytes".format(name, size))
+        print("")
+        return ("versions/{}".format(name))
+    return ("None")
+
+
+def do_deploy(archive_path):
+    """ deploy a file """
+    if (os.path.isfile(archive_path)):
+        com = "tar -xzf"
+        pth_re = "/data/web_static/releases/"
+        pth_cur = "/data/web_static/current"
+        fls = archive_path.split("/")
+        fld = fls[-1].split(".")
+
+        put(archive_path, "/tmp/")
+        sudo("mkdir -p {}{}".format(pth_re, fld[0]))
+        sudo("{} /tmp/{} -C {}{}/".format(com, fls[-1], pth_re, fld[0]))
+        sudo("rm /tmp/{}".format(fls[-1]))
+        sudo("mv {0}{1}/web_static/* {0}{1}".format(pth_re, fld[0]))
+        sudo("rm -rf {}{}/web_static".format(pth_re, fld[0]))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {}{} {}".format(pth_re, fld[0], pth_cur))
+        print("New version deployed!")
+        return True
+    else:
+        return False
+
+
+@task
 def deploy():
     path = do_pack()
     if (os.path.isfile(path)):
