d600dcb430da14c0aea330341e3c3d56c85cb4a4
MAZTRO <design.mallet@gmail.com>
2020-04-21 12:56:22 -0500

    Fix pep8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -36,15 +36,15 @@ def show_number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>')
-def html_number(n):
-    return render_template('5-number.html', number=n)
-
-
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def html_number(n):
     """ Method to print a template """
     return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', number=n)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
