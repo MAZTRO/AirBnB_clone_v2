07ed86721e36642f7ad94877393542895e8d7673
MAZTRO <design.mallet@gmail.com>
2020-04-21 12:29:53 -0500

    Fix Strict_Slashes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,9 +36,10 @@ def show_number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def html_number(n):
-    return render_template('5-number.html', number=n)
+     """ Method to print a template """
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
