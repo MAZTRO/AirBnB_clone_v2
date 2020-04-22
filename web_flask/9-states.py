3bac3cf4b02a3d85bbf9ff6b3b82504b1641b806
MAZTRO <design.mallet@gmail.com>
2020-04-22 01:44:35 +0000

    Add some files to Flask folder

---
+++ web_flask/9-states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states_list():
+    sts = storage.all('State')
+    return render_template('8-cities_by_states.html', states=sts)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
